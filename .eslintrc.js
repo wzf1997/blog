64d5f48e1fe6792c657ca76f5e30b0a91f5d192f
wzf1997 <582344150@qq.com>
2021-06-03 20:23:33 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//     "extends": ["eslint-config-ali"],
+//     "parser": "babel-eslint",
+//     "rules": {
+//       "prettier/prettier": "error",
+//       "strict": "off",
+//       "no-console": "off",
+//       "import/no-dynamic-require": "off",
+//       "global-require": "off",
+//       "require-yield": "off",
+//     },
+//     "plugins": ["prettier"],
+//     "globals": {
+//       "React": "readable"
+//     }
+//   };
--- set.js
+++ set.js
@@ -1,19 +1,18 @@
-
 (function () {
     let global = this;
-
     const makeIterator = function (array, iterator) {
         let nextIndex = 0;
         const obj = {
             next: function () {
-                return nextIndex < array.length ? { value: iterator(array[nextIndex++]), done: false } : { value: undefined, done: true };
+                return nextIndex < array.length ?
+                    { value: iterator(array[nextIndex++]), done: false } :
+                    { value: undefined, done: true };
             }
         };
         // [...set.keys()] 会调用这里
         obj[Symbol.iterator] = function () {
             return obj
         }
-
         return obj
     }
     class mySet {
@@ -27,22 +26,22 @@
             });
         }
 
-        get size() {
+        get size () {
             return this.data.length;
         }
 
-        [Symbol.iterator]() {
+        [Symbol.iterator] () {
             return this.values();
         }
 
-        add(value) {
+        add (value) {
             if (!this.data.includes(value)) {
                 this.data.push(value);
             }
             return this;
         }
 
-        delete(value) {
+        delete (value) {
             if (this.data.includes(value)) {
                 const index = this.data.findIndex(item => item === value);
                 this.data.splice(index, 1);
@@ -51,15 +50,15 @@
             return false;
         }
 
-        has(value) {
+        has (value) {
             return this.data.includes(value);
         }
 
-        clear() {
+        clear () {
             this.data.length = 0;
         }
 
-        forEach(callback, thisArg) {
+        forEach (callback, thisArg) {
             let context = thisArg || global;
             this.data.forEach(item => {
                 callback.call(context, item, item, this)
@@ -67,64 +66,18 @@
         }
 
 
-        keys() {
+        keys () {
             return makeIterator(this.data, value => value);
         }
-        values() {
+        values () {
             return this.keys();
         }
 
-        entires() {
+        entires () {
             return makeIterator(this.data, value => [value, value]);
         }
     }
+    
     global.mySet = mySet;
 })()
-
-// test 
-// let set = new mySet([1, 2, 3, 4, 4]);
-// console.log(set.size); // 4
-
-// set.delete(1);
-// console.log(set.has(1)); // false
-
-// set.clear();
-// console.log(set.size); // 0
-
-// set = new mySet([1, 2, 3, 4, 4]);
-// set.forEach((value, key, set) => {
-// 	console.log(value, key, set.size)
-// });
-
-let set = new mySet([1, 2, 3]);
-
-let set2 = new Set([1, 2, 3]);
-
-// console.log([...set2.keys()], [...set2.values()],[...set2.entries()], '9999')
-
-// console.log([...set.keys()], [...set.values()],[...set.entires()], '88888' )
-
-let set3 = new mySet([2,3]);
-let set4 = new mySet(set3);
-console.log(set3, set4);
-let set5 = new mySet(5);
-console.log(set5);
-
-
-// 迭代器
-function createIterator(items) {
-    let i = 0;
-    return {
-        next: function() {
-            let done = i >= items.length;
-            let value = !done ? items[i++] : undefined;
-            return {
-                done,
-                value
-            };
-        }
-    };
-}
-// test 一下
-const a = createIterator([1,2,3]);
-
+ 
